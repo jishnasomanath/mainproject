b8fdeb5acb02669154f7d8593cf7ad3f60a3e3c4
Jishna somanath <jishnasomanath@gmail.com>
2022-03-08 20:39:12 +0530

    First commit

---
+++ tables/db.sql
@@ -0,0 +1,78 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.03 
+MySQL - 8.0.21 : Database - blockhain
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`blockhain` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `blockhain`;
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `cust_id` int DEFAULT NULL,
+  `C_name` varchar(50) DEFAULT NULL,
+  `c_place` varchar(50) DEFAULT NULL,
+  `c_phone` bigint DEFAULT NULL,
+  `c_email` varchar(50) DEFAULT NULL,
+  `c_photo` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `customer` */
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `login_id` int NOT NULL AUTO_INCREMENT,
+  `userpassword` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `usertype` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `login` */
+
+/*Table structure for table `merchant` */
+
+DROP TABLE IF EXISTS `merchant`;
+
+CREATE TABLE `merchant` (
+  `merchant_id` int DEFAULT NULL,
+  `m_name` varchar(100) DEFAULT NULL,
+  `m_place` varchar(100) DEFAULT NULL,
+  `m_phone` bigint DEFAULT NULL,
+  `m_email` varchar(100) DEFAULT NULL,
+  `m_photo` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `merchant` */
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `p_id` int NOT NULL AUTO_INCREMENT,
+  `p_name` varchar(50) DEFAULT NULL,
+  `b_name` varchar(50) DEFAULT NULL,
+  `price` bigint DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`p_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `product` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
